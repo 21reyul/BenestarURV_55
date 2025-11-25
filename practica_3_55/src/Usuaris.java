994d87e23fd1796631783d13723f30f3ba744432
Cristina <ccozma000@gmail.com>
2025-11-25 16:45:50 +0100

    Classe usuari i subclasse

---
+++ practica_3_55/src/Usuaris.java
@@ -0,0 +1,40 @@
+public class Usuaris {
+    private String alies;
+    private String correu;
+
+    //Constructor
+    public Usuaris(String alies, String correu){
+        this.alies = alies;
+        this.correu = correu;
+    }
+
+    //Getter
+    public String getAlies(){
+        return alies;
+    }
+        
+    public String getCorreu(){
+        return correu;
+    }
+
+    //Setter
+        public void setAlies(String alies){
+        this.alies = alies;
+    }
+    
+    public void setCorreu(String correu){
+        this.correu = correu;
+    }
+
+    
+
+    
+
+    
+
+
+
+
+
+
+}
