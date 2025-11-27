517d44573d6cc2c45c95b2f2b79672b61149e6c9
Aroa Galvez <aroa.galvez@estudiats.urv.cat>
2025-11-27 11:09:16 +0100

    activitats y les seves subclasses

---
+++ practica_3_55/src/Activitats.java
@@ -0,0 +1,68 @@
+import java.time.LocalDate;
+
+public class Activitats {
+     private String nomActivitat;
+     private boolean PDI, PTGAS, Estudiants;
+     private LocalDate dataINI, dataFi;
+
+
+    public Activitats(String n, Boolean PDI, boolean PTGAS, boolean Estud, LocalDate dataI, LocalDate dataF){
+        nomActivitat=n; this.PDI=PDI; this.PTGAS=PTGAS; Estudiants=Estud; dataINI=dataI; dataFi=dataF;
+    }
+
+    public String getNomActivitat() {
+        return nomActivitat;
+    }
+
+    public void setNomActivitat(String nomActivitat) {
+        this.nomActivitat = nomActivitat;
+    }
+
+    public boolean isPDI() {
+        return PDI;
+    }
+
+    public void setPDI(boolean pDI) {
+        PDI = pDI;
+    }
+
+    public boolean isPTGAS() {
+        return PTGAS;
+    }
+
+    public void setPTGAS(boolean pTGAS) {
+        PTGAS = pTGAS;
+    }
+
+    public boolean isEstudiants() {
+        return Estudiants;
+    }
+
+    public void setEstudiants(boolean estudiants) {
+        Estudiants = estudiants;
+    }
+
+    public LocalDate getDataINI() {
+        return dataINI;
+    }
+
+    public void setDataINI(LocalDate dataINI) {
+        this.dataINI = dataINI;
+    }
+
+    public LocalDate getDataFi() {
+        return dataFi;
+    }
+
+    public void setDataFi(LocalDate dataFi) {
+        this.dataFi = dataFi;
+    }
+
+    @Override
+    public String toString() {
+        return "Activitats [nomActivitat=" + nomActivitat + ", PDI=" + PDI + ", PTGAS=" + PTGAS + ", Estudiants="
+                + Estudiants + ", dataINI=" + dataINI + ", dataFi=" + dataFi + "]";
+    }
+
+
+}
