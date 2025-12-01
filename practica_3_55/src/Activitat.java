7891af8ae2a4c14645fff3cf9b8a364e431c094a
Aina Garcia <aina.garciaa@estudiants.urv.cat>
2025-12-01 12:17:51 +0100

    Revert 'afegir classes activitat'

--- practica_3_55/src/Activitat.java
+++
@@ -1,8 +0,0 @@
-import java.time.LocalDate;
-
-public class Activitat {
-    protected String nomAct;
-    protected String ofertaColectiu;
-    protected LocalDate dataIniciInscripcions;   //TODO importar llibreria data
-    protected LocalDate dataFiInscripcions;      //TODO importar llibreria data
-}
