c335a1f3f840b1fb3c9eae8a8157e00343c6df52
Judit <judit.carles@estudiants.urv.cat>
2025-12-04 10:52:43 +0100

    main

--- practica_3_55/src/App.java
+++ practica_3_55/src/App.java
@@ -6,98 +6,101 @@ public class App {
 
 private void main(int opcio){
     boolean fi=false;
-    switch(opcio) {
-        case 1:
-            opcio1();
-            break;
+    while (!fi){
+        switch(opcio) {
+            case 1:
+                opcio1();
+                break;
 
-        case 2:
-            opcio2();
-            break;
+            case 2:
+                opcio2();
+                break;
 
-        case 3:
-            opcio3();
-            break;
+            case 3:
+                opcio3();
+                break;
 
-        case 4:
-            opcio4();
-            break;
+            case 4:
+                opcio4();
+                break;
 
-        case 5:
-            opcio5();
-            break;
+            case 5:
+                opcio5();
+                break;
 
-        case 6:
-            opcio6();
-            break;
+            case 6:
+                opcio6();
+                break;
 
-        case 7:
-            opcio7();
-            break;
+            case 7:
+                opcio7();
+                break;
 
-        case 8:
-            opcio8();
-            break;
+            case 8:
+                opcio8();
+                break;
 
-        case 9:
-            opcio9();
-            break;
+            case 9:
+                opcio9();
+                break;
 
-        case 10:
-            opcio10();
-            break;
+            case 10:
+                opcio10();
+                break;
 
-        case 11:
-            opcio11();
-            break;
+            case 11:
+                opcio11();
+                break;
 
-        case 12:
-            opcio12();
-            break;
+            case 12:
+                opcio12();
+                break;
 
-        case 13:
-            opcio13();
-            break;
+            case 13:
+                opcio13();
+                break;
 
-        case 14:
-            opcio14();
-            break;
+            case 14:
+                opcio14();
+                break;
 
-        case 15:
-            opcio15();
-            break;
+            case 15:
+                opcio15();
+                break;
 
-        case 16:
-            opcio16();
-            break;
+            case 16:
+                opcio16();
+                break;
 
-        case 17:
-            opcio17();
-            break;
+            case 17:
+                opcio17();
+                break;
 
-        case 18:
-            opcio18();
-            break;
+            case 18:
+                opcio18();
+                break;
 
-        case 19:
-            opcio19();
-            break;
+            case 19:
+                opcio19();
+                break;
 
-        case 20:
-            opcio20();
-            break;
+            case 20:
+                opcio20();
+                break;
 
-        case 21:
-            opcio21();
-            break;
+            case 21:
+                opcio21();
+                break;
 
-        case 22:
-            opcio22();
-            break;
+            case 22:
+                fi=true;
+                System.out.println("S'ortint del programa");
+                break;
 
-        default:
-            System.out.println("Opción no válida");
-            break;
+            default:
+                System.out.println("Opción no válida");
+                break;
+        }
     }
 }
 
@@ -185,6 +188,4 @@ private void opcio21(){
 
 }
 
-private void opcio22(){
 
-}
